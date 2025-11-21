05ff528e7fca6355354785235c8a9af4f4e86ef8
Kovid <kovid611@gmail.com>
2025-11-22 02:28:52 +0530

    Move images to public/images folder

--- src/data/products.js
+++ src/data/products.js
@@ -1,7 +1,7 @@
 export const products = [
-  { id: 1, title: 'Dragon T-shirt', price: 'INR 450', image: '/image 130.png' },
-  { id: 2, title: 'Dragon T-shirt', price: 'INR 450', image: '/image 130.png' },
-  { id: 3, title: 'Dragon T-shirt', price: 'INR 450', image: '/image 130.png' },
-  { id: 4, title: 'Dragon T-shirt', price: 'INR 450', image: '/image 130.png' },
-  { id: 5, title: 'Dragon T-shirt', price: 'INR 450', image: '/image 130.png' }
+  { id: 1, title: 'Dragon T-shirt', price: 'INR 450', image: '/images/image 130.png' },
+  { id: 2, title: 'Dragon T-shirt', price: 'INR 450', image: '/images/image 130.png' },
+  { id: 3, title: 'Dragon T-shirt', price: 'INR 450', image: '/images/image 130.png' },
+  { id: 4, title: 'Dragon T-shirt', price: 'INR 450', image: '/images/image 130.png' },
+  { id: 5, title: 'Dragon T-shirt', price: 'INR 450', image: '/images/image 130.png' }
 ]
